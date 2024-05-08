9f4e5144bc03fc5bac2a24774505647aa3bb9784
seryh <seryh@DESKTOP-6PAI48T>
2024-05-08 20:47:52 +0300

    Добавьте файлы проекта.

---
+++ MyBlogApp.Server/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBlogApp.Server.Models;
+
+namespace MyBlogApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserModel user)
+        {
+            // add user to DB
+
+            return Ok(user);
+        }
+
+        [HttpPatch]
+        public ActionResult<UserModel> Update(UserModel user)
+        {
+            // check current user from request with user model
+            // update user in DB
+
+            return Ok(user);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult GetToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
